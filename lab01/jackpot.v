7283dc3de1acc83727f557e9ee84efe1e02b7ed6
root <root@apache.localdomain>
2020-01-26 00:15:02 +0000

    lab01 updates

--- lab01/jackpot.v
+++ lab01/jackpot.v
@@ -2,43 +2,46 @@
 module jackpot(
     input [3:0] SWITCHES,
     output [3:0] LEDS,
-    input CLOCK
+    input CLOCK,
+    input RESET
     );
-    
 
-    
-    reg [23:0] divider;
-    reg[3:0] outs;
-    reg won = 1'b0;
-    
+    reg [23:0] divider;        //Clock division reg
+    reg[3:0] outs;            //LED output reg
+    reg won = 1'b0;            //True if player has won
     
     
-    always@(posedge CLOCK) begin
     
-        if (won) begin
-            if (divider[20]) begin
-                    outs <= 4'b1111;
-            end
-            else begin
-                outs <= 4'b0000;
-            end
-        end
+    always@(posedge CLOCK or posedge RESET) begin
     
-        divider <= divider + 1;
+        divider <= divider + 1; //Increment the clock divider
         
-        if (won == 1'b0 & divider == 4'hFFF) begin
+        //One hot fashion logic using LSL
+        if (won == 1'b0 & divider == 3'hFFF) begin
             outs <= outs << 1;
         end
-        
-        if(won == 1'b0 & outs == 4'b0000) outs = 4'b0001;
-        
-    end
-    
-    always@(posedge SWITCHES) begin
-        if((outs & SWITCHES) > 0) begin
-            won <= 1'b1;
+        //Reset one hot logic
+        if(won == 1'b0 & outs == 4'b0000) begin
+            outs <= 4'b0001;
         end
+        //Manual reset
+        if(RESET == 1) begin
+            outs <= 4'b0001;
+        end
+        
+        if(won == 1'b1) begin
+            outs <= 4'b1111;
+        end
+        
     end
     
+    always@(posedge SWITCHES or posedge RESET) begin
+        if(SWITCHES[3:0] == outs[3:0]) begin
+            won <= 1'b1;
+        else
+            won <= 1'b0;    //Triggered during RESET as well!
+    end
+    
+    //Display one hot LEDs
     assign LEDS[3:0] = outs[3:0];
 endmodule
