1085bfd7506cc1cd122d6344d4b82de13613e05c
Philip Smith <philipsmith@n03-poseidon.olympus.ece.tamu.edu>
2020-02-16 12:19:17 -0600

    t

---
+++ lab04/u-boot/include/generated/version_autogenerated.h
@@ -0,0 +1,4 @@
+#define PLAIN_VERSION "2014.01"
+#define U_BOOT_VERSION "U-Boot 2014.01"
+#define CC_VERSION_STRING "arm-xilinx-linux-gnueabi-gcc (Sourcery CodeBench Lite 2014.11-30) 4.9.1"
+#define LD_VERSION_STRING "GNU ld (Sourcery CodeBench Lite 2014.11-30) 2.24.51.20140217"
