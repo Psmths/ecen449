9c7e11e479108c2867f7c0a18abde0a4651659e1
philipsmith <philipsmith@tamu.edu>
2020-02-16 11:14:46 -0600

    lab4 progress

--- lab03/lab03.runs/impl_1/runme.sh
+++ lab03/lab03.runs/impl_1/runme.sh
@@ -37,7 +37,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
+/bin/touch .write_bitstream.begin.rst
 EAStep vivado -log multiply_wrapper.vdi -applog -m64 -messageDb vivado.pb -mode batch -source multiply_wrapper.tcl -notrace
 
 
